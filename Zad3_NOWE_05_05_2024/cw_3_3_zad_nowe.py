130389f850598598653f17852b9f3c4c3905ea48
mateusz90ba <163890284+mateusz90ba@users.noreply.github.com>
2024-05-05 16:02:02 +0200

    Add files via upload

---
+++ Zad3_NOWE_05_05_2024/cw_3_3_zad_nowe.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+import pandas as pd
+from tkinter import Tk, filedialog
+from skimage import io, color
+from skimage.util import img_as_ubyte
+from skimage.feature import graycomatrix, graycoprops
+
+print("Program do wyznaczania cech struktury dla danych trzech tekstur")
+print("W widocznym oknie wybierz 3 foldery, w których zapisane są fragmenty tekstur, aby utworzyć z nich jeden plik CSV")
+
+def compute_texture_features(image, distances, angles):
+    features = []
+    if len(image.shape) == 3 and image.shape[2] == 4:  #Sprawdź, czy obraz ma kanał alfa (RGBA)
+        image = image[:, :, :3]  #Ignoruj kanał alfa
+
+    gray_image = color.rgb2gray(image)
+    gray_image = img_as_ubyte(gray_image)  #Zmniejszenie głębi jasności do 8 bitów (256 poziomów)
+
+    #Obliczenie macierzy zdarzeń dla każdej kombinacji odległości i kierunku
+    for d in distances:
+        for a in angles:
+            glcm = graycomatrix(gray_image, distances=[d], angles=[a], levels=256, symmetric=True, normed=True)
+            props = [graycoprops(glcm, prop)[0, 0] for prop in ['dissimilarity', 'correlation', 'contrast', 'energy', 'homogeneity', 'ASM']]
+            features.extend(props)
+
+    return features
+
+def process_texture_samples():
+    root = Tk()
+    root.withdraw()  # Ukryj główne okno tkinter
+
+    input_folders = []
+    for _ in range(3):
+        folder = filedialog.askdirectory()  # Wybierz folder za pomocą okna dialogowego
+        if folder:
+            input_folders.append(folder)
+
+    if not input_folders:
+        print("Nie wybrano folderów.")
+        return
+
+    data = []
+
+    for input_folder in input_folders:
+        # Przetwarzanie każdego obrazu w folderze
+        for filename in os.listdir(input_folder):
+            if filename.endswith(".png"):
+                image_path = os.path.join(input_folder, filename)
+                image = io.imread(image_path)
+                features = compute_texture_features(image, distances=[1, 3, 5], angles=[0, np.pi / 4, np.pi / 2, 3 * np.pi / 4])
+                category = os.path.basename(input_folder)  # Kategoria tekstury to nazwa folderu
+                data.append(features + [category])
+
+    # Tworzenie ramki danych Pandas i zapis do jednego pliku CSV
+    columns = [f"{prop}_{d}_{a}" for d in [1, 3, 5] for a in [0, 45, 90, 135] for prop in
+               ['dissimilarity', 'correlation', 'contrast', 'energy', 'homogeneity', 'ASM']] + ['category']
+    df = pd.DataFrame(data, columns=columns)
+
+    # Zapis do jednego pliku CSV
+    output_file = os.path.join(os.path.commonpath(input_folders), "wynik.csv")
+    df.to_csv(output_file, index=False)
+
+    print(f"Plik CSV został zapisany w folderze: {output_file}")
+
+# Uruchomienie programu
+process_texture_samples()
